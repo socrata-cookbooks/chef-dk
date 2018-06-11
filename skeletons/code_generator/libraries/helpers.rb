5d12a258a4cfd1461749420cdc4311461c1e1ad0
Jonathan Hartman <j@hartman.io>
2018-06-11 10:41:56 -0700

    Expand the default cookbook readme

--- skeletons/code_generator/libraries/helpers.rb
+++ skeletons/code_generator/libraries/helpers.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+require 'mixlib/shellout'
+
 {
   copyright_holder: 'Socrata, Inc.',
   email: 'sysadmin@socrata.com',
   github_user: 'socrata-shared',
   github_org: 'socrata-cookbooks',
   supermarket_user: 'socrata',
+  maintainer_name: Mixlib::ShellOut.new('git config user.name').run_command
+                                                               .stdout.strip,
+  maintainer_email: Mixlib::ShellOut.new('git config user.email').run_command
+                                                                 .stdout.strip,
   use_berkshelf: true
 }.each do |k, v|
   ChefDK::Generator.add_attr_to_context(k, v)

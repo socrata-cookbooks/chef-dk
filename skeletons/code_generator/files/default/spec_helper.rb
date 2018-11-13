570fae3c31442340559ccbddb77ace0969945d95
Jonathan Hartman <j@hartman.io>
2018-11-13 12:34:30 -0800

    Remove platforms from the spec_helper

--- skeletons/code_generator/files/default/spec_helper.rb
+++ skeletons/code_generator/files/default/spec_helper.rb
@@ -5,12 +5,6 @@ require 'chefspec/berkshelf'
 require 'simplecov'
 require 'simplecov-console'
 
-RSpec.configure do |c|
-  c.add_setting :supported_platforms, default: {
-    ubuntu: %w[18.04 16.04 14.04]
-  }
-end
-
 SimpleCov.formatter = SimpleCov::Formatter::Console
 SimpleCov.minimum_coverage(100)
 SimpleCov.start
